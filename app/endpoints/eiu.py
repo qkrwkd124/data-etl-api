6498848d86cc2059abfc8c37ec9d83828004f1d5
qkrwkd124 <qkrwkd120@naver.com>
2025-07-08 13:22:30 +0900

    데이터베이스 추가

--- app/endpoints/eiu.py
+++ app/endpoints/eiu.py
@@ -3,16 +3,16 @@ from typing import Dict, Any
 from fastapi import APIRouter, HTTPException
 from fastapi import Depends
 
-from app.db.base import get_main_db
-from app.schemas.api_schemas import EIUUploadRequest, EIUUploadResponse
+from app.db.base import get_main_db, get_dbpdtm_db
+from app.schemas.api_schemas import UploadRequest, UploadResponse
 from app.services.eiu_service import process_eiu_economic_indicator
 from app.services.major_trade_partner_service import process_eiu_major_trade_partner
 
 router = APIRouter()
 
-@router.post("/eiu/economic-indicator/upload", response_model=EIUUploadResponse)
+@router.post("/eiu/economic-indicator/upload", response_model=UploadResponse)
 async def process_data(
-    request: EIUUploadRequest,
+    request: UploadRequest,
     dbprsr = Depends(get_main_db)
 ) -> Dict[str, Any]:
     """
@@ -34,21 +34,22 @@ async def process_data(
             replace_all=True
         )
             
-        return EIUUploadResponse(
+        return UploadResponse(
             success=True,
             message="데이터 처리 완료",
         )
         
     except Exception as e:
-        return EIUUploadResponse(
+        return UploadResponse(
             success=False,
             message="데이터 처리 실패",
         )
     
-@router.post("/eiu/major-trade-partner/upload", response_model=EIUUploadResponse)
+@router.post("/eiu/major-trade-partner/upload", response_model=UploadResponse)
 async def process_major_trade_partner(
-    request: EIUUploadRequest,
-    dbprsr = Depends(get_main_db)
+    request: UploadRequest,
+    dbprsr = Depends(get_main_db),
+    dbpdtm = Depends(get_dbpdtm_db)
 ) -> Dict[str, Any]:
     """
     EIU 주요 수출입국 엑셀 파일을 처리하는 엔드포인트
@@ -65,16 +66,17 @@ async def process_major_trade_partner(
             file_path=request.file_path,
             file_name=request.file_name,
             dbprsr=dbprsr,
+            dbpdtm=dbpdtm,
             replace_all=True
         )
         
-        return EIUUploadResponse(
+        return UploadResponse(
             success=True,
             message=f"주요 수출입국 데이터 처리 완료 ({result_df.shape[0]}행 처리됨)",
         )
         
     except Exception as e:
-        return EIUUploadResponse(
+        return UploadResponse(
             success=False,
             message=f"주요 수출입국 데이터 처리 실패: {str(e)}",
         )
