c58bd11836a33c0de178154b0892016859a65fe1
qkrwkd124 <qkrwkd120@naver.com>
2025-07-08 13:22:39 +0900

    중복 테이블 제거

--- app/models/customs.py
+++ app/models/customs.py
@@ -13,16 +13,16 @@ class ExportImportStatByCountry(Base):
     impexp_imp_money = Column(String(30), comment="수입금액")
     impexp_trade_rate_money = Column(String(30), comment="무역수지")
 
-class CountryMapping(Base):
-    __tablename__ = "tb_rhr160"
+# class CountryMapping(Base):
+#     __tablename__ = "tb_rhr160"
 
-    iso_code = Column(String(4), primary_key=True, comment="ISO코드")
-    nation_nm = Column(String(200), comment="국가명")
-    trgtpsn_no = Column(String(30), comment="대상자번호")
+#     iso_code = Column(String(4), primary_key=True, comment="ISO코드")
+#     nation_nm = Column(String(200), comment="국가명")
+#     trgtpsn_no = Column(String(30), comment="대상자번호")
 
-class KoreaCountryMapping(Base):
-    __tablename__ = "tb_rhr350"
+# class KoreaCountryMapping(Base):
+#     __tablename__ = "tb_rhr350"
 
-    eng_ctry_nm = Column(String(200), primary_key=True, nullable=True, comment="영문국가명")
-    std_infrm_ctry_cd = Column(String(2), primary_key=True, nullable=True, comment="표준약식국가코드")
-    kcs_kor_ctry_nm = Column(String(100), comment="관세청한글국가명")
+#     eng_ctry_nm = Column(String(200), primary_key=True, nullable=True, comment="영문국가명")
+#     std_infrm_ctry_cd = Column(String(2), primary_key=True, nullable=True, comment="표준약식국가코드")
+#     kcs_kor_ctry_nm = Column(String(100), comment="관세청한글국가명")
