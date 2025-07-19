18386c67f8f59de8989e758a0ad2b629f0e5e463
qkrwkd124 <qkrwkd120@naver.com>
2025-07-19 16:54:25 +0900

    관세청 품목데이터 모델 추가

--- app/models/customs.py
+++ app/models/customs.py
@@ -6,6 +6,12 @@ from datetime import datetime
 class ExportImportStatByCountry(Base):
     __tablename__ = "tb_rhr140"
 
+    __table_args__ = {
+        "comment": "대한민국 주요 수출/수입량",
+        "mysql_charset": "utf8mb4",
+        "mysql_collate": "utf8mb4_unicode_ci"
+    }
+
     impexp_year = Column(String(4), primary_key=True, comment="수출입연도")
     impexp_nation_code = Column(String(2), primary_key=True, comment="수출입국가코드")
     impexp_nation_nm = Column(String(150), comment="수출입국가명")
@@ -13,16 +19,19 @@ class ExportImportStatByCountry(Base):
     impexp_imp_money = Column(String(30), comment="수입금액")
     impexp_trade_rate_money = Column(String(30), comment="무역수지")
 
-# class CountryMapping(Base):
-#     __tablename__ = "tb_rhr160"
+class ExportImportItemByCountry(Base):
+    __tablename__ = "tb_rhr130"
 
-#     iso_code = Column(String(4), primary_key=True, comment="ISO코드")
-#     nation_nm = Column(String(200), comment="국가명")
-#     trgtpsn_no = Column(String(30), comment="대상자번호")
+    __table_args__ = {
+        "comment": "대한민국 주요 수출/수입국",
+        "mysql_charset": "utf8mb4",
+        "mysql_collate": "utf8mb4_unicode_ci"
+    }
 
-# class KoreaCountryMapping(Base):
-#     __tablename__ = "tb_rhr350"
-
-#     eng_ctry_nm = Column(String(200), primary_key=True, nullable=True, comment="영문국가명")
-#     std_infrm_ctry_cd = Column(String(2), primary_key=True, nullable=True, comment="표준약식국가코드")
-#     kcs_kor_ctry_nm = Column(String(100), comment="관세청한글국가명")
+    impexp_year = Column(String(4), primary_key=True, comment="수출입연도")
+    impexp_flag = Column(String(10), primary_key=True, comment="수출입구분")
+    impexp_nation_code = Column(String(2), primary_key=True, comment="수출입국가코드")
+    impexp_nation_nm = Column(String(150), primary_key=True, comment="수출입국가명")
+    impexp_item_nm = Column(String(150), primary_key=True, comment="수출입품명")
+    impexp_item_weight = Column(String(150), comment="수출입품무게")
+    impexp_item_money = Column(String(150), comment="수출입품금액")
