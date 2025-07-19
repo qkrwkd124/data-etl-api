e4558ba5d3d2095f3c309bb682f463eaef585ea4
qkrwkd124 <qkrwkd120@naver.com>
2025-07-19 16:54:52 +0900

    시퀀스 request 추가

--- app/schemas/api_schemas.py
+++ app/schemas/api_schemas.py
@@ -4,6 +4,9 @@ class UploadRequest(BaseModel):
     file_path: str = Field(..., description="파일 경로")
     file_name: str = Field(..., description="파일 이름")
 
+class UploadRequest2(BaseModel):
+    seq: str = Field(..., description="순번")
+
 class UploadResponse(BaseModel):
     success: bool = Field(..., description="성공 여부")
     message: str = Field(..., description="메시지")
